42dc6bbbb0a427a399e6a9fbc24dbd3e5f4bd5c4
omarinux <81483478+omarinux@users.noreply.github.com>
2023-12-03 13:11:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
 locals {
   foldername = "test02222"
+  foldername33 = "333333"
 }
 
 resource "null_resource" "makefolder" {
 
   provisioner "local-exec" {
     command = "rm -rf /tmp/${local.foldername} && /usr/bin/mkdir /tmp/${local.foldername} && touch /tmp/${local.foldername}.txt && echo 'ciao' >> /tmp/${local.foldername}.txt"
+  }
+}
+
+resource "null_resource" "makefolder33" {
+
+  provisioner "local-exec" {
+    command = "rm -rf /tmp/${local.foldername33} && /usr/bin/mkdir /tmp/${local.foldername33} && touch /tmp/${local.foldername33}.txt && echo 'ciao' >> /tmp/${local.foldername33}.txt"
   }
 }
